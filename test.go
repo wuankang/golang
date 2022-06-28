565bb8fa78b97ab9fd718ddbe78e836d84e0f4ab
wuankang <1287147830@qq.com>
2022-06-28 23:03:40 +0800

    submit

--- test.go
+++ test.go
@@ -1,7 +1,16 @@
 package main 
 
-import "fmt"
+import(
+	"fmt"
+)
+/*func main(){
+	fmt.Println("hello world")
+}*/
 
 func main(){
-	fmt.Println("hello world")
+	var num1 int = 1199
+	var num2 int = 1200
+	var d1 float64
+	d1 = float64(num1)/float64(num2)
+	fmt.Printf("%.2f\n",d1)
 }
